51f2f6beada389f5e314311ee87ed789d83cca73
[anudeep avula] <[anudeepavula009@gmail.com]>
2024-12-13 20:10:24 +0530

    Fix bug related to sticky breadcrumbs

---
+++ src/components/FolderView.tsx
@@ -0,0 +1,72 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import { ContentCard } from './ContentCard';
+import { courseContent, getFilteredContent } from '@/lib/utils';
+import { useRecoilValue } from 'recoil';
+import { selectFilter } from '@/store/atoms/filterContent';
+
+export const FolderView = ({
+  courseContent,
+  courseId,
+  rest,
+}: {
+  courseId: number;
+  rest: string[];
+  courseContent: courseContent[];
+}) => {
+  const router = useRouter();
+
+  if (!courseContent?.length) {
+    return (
+      <div className="mt-64 flex">
+        <div className="m-auto">No content here yet!</div>
+      </div>
+    );
+  }
+  let updatedRoute = `/courses/${courseId}`;
+  for (let i = 0; i < rest.length; i++) {
+    updatedRoute += `/${rest[i]}`;
+  }
+  // why? because we have to reset the segments or they will be visible always after a video
+
+  const currentfilter = useRecoilValue(selectFilter);
+
+  const filteredCourseContent = getFilteredContent(
+    courseContent,
+    currentfilter,
+  );
+
+  return (
+    <div>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {filteredCourseContent.map((content) => {
+          const videoProgressPercent =
+            content.type === 'video' &&
+              content.videoFullDuration &&
+              content.duration
+              ? (content.duration / content.videoFullDuration) * 100
+              : content.percentComplete || 0;
+
+          return (
+            <ContentCard
+              type={content.type}
+              contentId={content.id}
+              key={content.id}
+              title={content.title}
+              image={content.image || ''}
+              onClick={() => {
+                router.push(`${updatedRoute}/${content.id}`);
+              }}
+              markAsCompleted={content.markAsCompleted}
+              percentComplete={content.percentComplete}
+              videoProgressPercent={videoProgressPercent}
+              bookmark={content.bookmark}
+              contentDuration={content.videoFullDuration}
+              weeklyContentTitles={content.weeklyContentTitles}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
